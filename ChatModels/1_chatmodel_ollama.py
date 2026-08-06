f5635965542bb6a5d4ca9f7781e77dcbfcca5ce0
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-08-07 03:36:00 +0530

    Revising Langchain

--- ChatModels/1_chatmodel_ollama.py
+++ ChatModels/1_chatmodel_ollama.py
@@ -3,7 +3,7 @@ from langchain_community.chat_models import ChatOllama
 model = ChatOllama(model="mistral",
                    model_kwargs={"temperature": 0.7,
                                  "num_predict": 10}
-                   )  # With open AI and other models, temperature attribute ranging from 0-2 yield creative output in increasing order
+                   )  # With open AI and other models, temperature attribute ranging from 0-1 yield creative output in increasing order
 
 result = model.invoke("Write a poem on the topic, 'Guitar' ")
 
